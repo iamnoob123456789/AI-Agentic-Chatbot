62630555b07708262ac83883b52f4c3c60b921f8
iamnoob123456789 <shreyasakula97@gmail.com>
2026-01-13 21:00:31 +0530

    all

--- src/langgraphagenticai/state/state.py
+++
@@ -1,7 +0,0 @@
-from typing_extensions import TypedDict,list
-from langgraph.graph.message import add_messages
-class State(TypeDict):
-    """
-      Represent the structure of the state used in graph
-    """
-    message:Annotated[list,add_messages]
